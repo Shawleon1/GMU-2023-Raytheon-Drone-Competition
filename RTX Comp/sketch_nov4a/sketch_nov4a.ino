814a0fd8e1c97e0c345427fa42374c55132fd9b1
ibirana7 <ibirana7@gmail.com>
2023-11-07 15:18:11 -0500

    LED Test

--- RTX Comp/sketch_nov4a/sketch_nov4a.ino
+++ RTX Comp/sketch_nov4a/sketch_nov4a.ino
@@ -1,9 +1,32 @@
+int led = 13;
+int input = -1;
+int button = 1;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT;
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int pulse;
 
+  if(input == -1){
+    pulse = 1000;
+  }
+
+  if(input == 0){
+    pulse = 1500;
+  }
+
+  if(input == 1){
+    pulse = 2000;
+  }
+
+  digitalWrite(led, LOW);
+  delay(pulse);
+  digitalWrite(led,HIGH);
+  delay(pulse);
 }
