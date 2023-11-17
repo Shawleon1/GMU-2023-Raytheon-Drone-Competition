9db5f18e7edff376fd171c001310fcd479a11053
Shawleon1 <103335425+Shawleon1@users.noreply.github.com>
2023-11-16 19:46:29 -0500

    last commit before we overrite the code with stuff from Aniket I guess

--- RTX Comp/sketch_nov4a/sketch_nov4a.ino
+++ RTX Comp/sketch_nov4a/sketch_nov4a.ino
@@ -17,18 +17,31 @@ static const int STEERING_IN = 6;
 static const int THROTTLE_OUT = 9;
 static const int STEERING_OUT = 10;
 
-float input = 1;
+static const int NUM_CHANNELS = 2;
+static const int PWM_IN_DISCONNECT = 700;
+static const int PWM_OUT_DISCONNECT = 1500;
+
+float channel[NUM_CHANNELS];
 int button = 1;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(THROTTLE_OUT, OUTPUT);
+  pinMode(STEERING_OUT, OUTPUT);
+
+  pinMode(THROTTLE_IN, INPUT);
+  pinMode(THROTTLE_IN, INPUT);
   pinMode(button, INPUT);
 
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  channel[0] = pulseIn(THROTTLE_IN, HIGH);
+  channel[1] = pulseIn(STEERING_IN, HIGH);
+  
   int pulse;
 
   if(input < 0 && input >= -1){
