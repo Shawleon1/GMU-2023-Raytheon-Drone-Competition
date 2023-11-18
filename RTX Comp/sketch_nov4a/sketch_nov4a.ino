5954321f7140db3d97a46ff619bc21827299aab3
Shawleon1 <103335425+Shawleon1@users.noreply.github.com>
2023-11-18 15:09:19 -0500

    Steering via terminal input

--- RTX Comp/sketch_nov4a/sketch_nov4a.ino
+++ RTX Comp/sketch_nov4a/sketch_nov4a.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
+
+Servo steeringServo;
 /*
-Servo myservo;
 const int steeringPin = 2;
 const int drivePin = 3;
 
@@ -11,53 +12,42 @@ void setup() {
 }
 
 */
-static const int THROTTLE_IN = 5;
-static const int STEERING_IN = 6;
+// static const int THROTTLE_IN = 5;
+// static const int STEERING_IN = 2;
 
-static const int THROTTLE_OUT = 9;
-static const int STEERING_OUT = 10;
+// static const int THROTTLE_OUT = 9;
+static const int STEERING_OUT = 2;
+float servoval = 500.0;
+static const int NUM_CHANNELS = 1;
+// static const int PWM_IN_DISCONNECT = 700;
+// static const int PWM_OUT_DISCONNECT = 1500;
 
-static const int NUM_CHANNELS = 2;
-static const int PWM_IN_DISCONNECT = 700;
-static const int PWM_OUT_DISCONNECT = 1500;
-
-float channel[NUM_CHANNELS];
-int button = 1;
+// // float channel[NUM_CHANNELS];
+// int button = 1;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(THROTTLE_OUT, OUTPUT);
-  pinMode(STEERING_OUT, OUTPUT);
-
-  pinMode(THROTTLE_IN, INPUT);
-  pinMode(THROTTLE_IN, INPUT);
-  pinMode(button, INPUT);
-
+  steeringServo.attach(STEERING_OUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  channel[0] = pulseIn(THROTTLE_IN, HIGH);
-  channel[1] = pulseIn(STEERING_IN, HIGH);
-  
-  int pulse;
-
-  if(input < 0 && input >= -1){
-    pulse = 1000;
-    pulse += 500 * (input * -1);
-
+  while (Serial.available()) {
+    servoval = Serial.parseFloat();
+    while(Serial.read() == '\n');
+    Serial.print("input value");
+    Serial.println(servoval);
+    delay(500);
+    servoval = servoval * 1000;
+    Serial.print("multiply value");
+    Serial.println(servoval);
+    delay(500);
+    servoval = servoval + 1000;
   }
+  Serial.println(servoval);
 
-  if(input >= 0 && input <= 1){
-    pulse = 1500;
-    pulse += 500 * (input * -1);
-  }
-
-  digitalWrite(THROTTLE_OUT, LOW);
-  delay(pulse);
-  digitalWrite(THROTTLE_OUT,HIGH);
-  delay(pulse);
-
+  steeringServo.writeMicroseconds(servoval);
+  delay(500);
 }
+
