d75663b2671ee23f7073811b98683572a115f168
Shawleon1 <103335425+Shawleon1@users.noreply.github.com>
2023-11-19 13:06:57 -0500

    renaming

--- RTX Comp/ROS code/rostest.py
+++ RTX Comp/ROS code/rostest.py
@@ -3,18 +3,18 @@
 import rospy
 from std_msgs.msg import Float32MultiArray
 
-def talker():
+def rostest():
     pub = rospy.Publisher("/cmd_vel1", Float32MultiArray, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    rospy.init_node('rostest', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        servo_values = [0, 1, 0, 0, 0, 0]
+        servo_values = [.75, 1, 0, 0, 0, 0]
         rospy.loginfo(servo_values)
-        pub.publish(servo_values)
+        pub.publish(6, servo_values)
         rate.sleep()
 
 if __name__ == '__main__':
     try:
-        talker()
+        rostest()
     except rospy.ROSInterruptException:
         pass
