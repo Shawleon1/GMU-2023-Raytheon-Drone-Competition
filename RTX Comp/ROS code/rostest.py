a7d6edd9a12dd317f1fe70ed610c9077212f63fe
Shawleon1 <103335425+Shawleon1@users.noreply.github.com>
2023-11-19 14:11:37 -0500

    Publishing to cmd_vel1

--- RTX Comp/ROS code/rostest.py
+++ RTX Comp/ROS code/rostest.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python
-# license removed for brevity
+#!/usr/bin/env python3
+
 import rospy
 from std_msgs.msg import Float32MultiArray
 
-def rostest():
-    pub = rospy.Publisher("/cmd_vel1", Float32MultiArray, queue_size=10)
-    rospy.init_node('rostest', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        servo_values = [.75, 1, 0, 0, 0, 0]
-        rospy.loginfo(servo_values)
-        pub.publish(6, servo_values)
-        rate.sleep()
-
-if __name__ == '__main__':
+if name == 'main':
     try:
-        rostest()
+        servo_values = Float32MultiArray(data=[-1.0, 1.0, 0.0, 0.0, 0.0, 0.0])
+        pub = rospy.Publisher("/cmd_vel1", Float32MultiArray, queue_size=10)
+        rospy.init_node('ros__test')
+        rospy.loginfo("ros_test is started")
+        rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+            servo_values.data[0] = servo_values.data[0] + 0.01
+            servo_values.data[1] = servo_values.data[1] - 0.01
+            rospy.loginfo(servo_values)
+            pub.publish(servo_values)
+            rate.sleep()
     except rospy.ROSInterruptException:
         pass
