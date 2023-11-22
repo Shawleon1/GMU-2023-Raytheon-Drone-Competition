0e629454efdce3870a8528c86a3cda3be2b92011
Muhammad Anjum <ibirana7@comp.fios-router.home>
2023-11-21 20:35:43 -0500

    Adding Catkin Workspace

---
+++ RTX Comp/ROS code/catkin_ws/build/testpkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ibirana7/catkin_ws/src/testpkg
+# Build directory: /home/ibirana7/catkin_ws/build/testpkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ RTX Comp/ROS code/catkin_ws/src/testpkg/scripts/rostest.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32MultiArray
+from sensor_msgs.msg import Joy
+
+steering = 0
+throttle = 0
+
+def joy_callback(msg):
+    global steering, throttle
+    steering = msg.axes[0]
+    throttle = msg.axes[4]
+
+if __name__ == '__main__':
+    try:
+        servo_values = Float32MultiArray(data=[-1.0, 1.0, 0.0, 0.0, 0.0, 0.0])
+
+        #accessing joystick (in the future will be motion planner) and publishing to arduino
+        pub = rospy.Publisher("/cmd_vel1", Float32MultiArray, queue_size=10)
+        sub = rospy.Subscriber("/joy", Joy, joy_callback)
+
+        rospy.init_node('ros__test')
+        rospy.loginfo("ros_test is started")
+        rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+            servo_values.data[0] = steering
+            servo_values.data[1] = throttle
+            rospy.loginfo(servo_values)
+            pub.publish(servo_values)
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
